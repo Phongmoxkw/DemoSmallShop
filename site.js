a85dbaec8dc6e0e80179102b800204a8ee3ef538
Phongmoxkw <109543095+Phongmoxkw@users.noreply.github.com>
2023-01-24 22:13:51 +0700

    Add files via upload

---
+++ site.js
@@ -0,0 +1,22 @@
+
+    function myFunction() {
+        var x = document.getElementById("hien");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+      function myFunction2() {
+        var x = document.getElementById("hien");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+       
+        } else {
+          x.style.display = "block";
+        
+        }
+      }
+    
+    
